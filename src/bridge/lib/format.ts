59e7243c174bb729baaf0aa23634592e06c87375
Ash <ashley@juice49.com>
2023-09-17 22:18:42 +0100

    feat: include URL details in output

--- src/bridge/lib/format.ts
+++ src/bridge/lib/format.ts
@@ -1,10 +1,11 @@
 import { type Groqfmt, type GroqfmtResult } from '../../groqfmt/types'
+import getQueryDetails, { type Details } from './get-query-details'
 import getQueryParams from './get-query-params'
 
 /** @public */
 export type GroqfmtResultEnhanced = GroqfmtResult & {
   params?: Record<string, string>
-}
+} & Partial<Details>
 
 /** @public */
 export function format({
@@ -24,6 +25,7 @@ export function format({
       if (query) {
         return {
           ...groqfmt(query),
+          ...getQueryDetails(url),
           params: getQueryParams(url),
         }
       }
